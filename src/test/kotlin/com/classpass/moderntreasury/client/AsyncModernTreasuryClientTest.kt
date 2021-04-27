5848535282b8c56e526b52cce3aea7137aef990f
Zander Wilson-Thompson <zwilson-thompson@classpass.com>
2021-04-26 18:36:44 -0600

    Wiremock test to verify we're including basic auth

---
+++ src/test/kotlin/com/classpass/moderntreasury/client/AsyncModernTreasuryClientTest.kt
@@ -0,0 +1,68 @@
+package com.classpass.moderntreasury.client
+
+import com.classpass.moderntreasury.config.ModernTreasuryConfig
+import com.classpass.moderntreasury.config.ModernTreasuryModule
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.BasicCredentials
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.google.inject.Guice
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+
+private const val ORG_ID = "org_id"
+private const val API_KEY = "api_key"
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AsyncModernTreasuryClientTest {
+
+    private val wireMockServer = WireMockServer(WireMockConfiguration.options().dynamicPort())
+    private lateinit var client: ModernTreasuryClient
+
+    @BeforeAll
+    fun beforeAll() {
+        wireMockServer.start()
+        val baseUrl = "http://localhost:${wireMockServer.port()}"
+        val config = ModernTreasuryConfig(ORG_ID, API_KEY, baseUrl)
+        client = Guice.createInjector(ModernTreasuryModule(config)).getInstance(ModernTreasuryClient::class.java)
+
+        configureFor("localhost", wireMockServer.port())
+    }
+
+    @BeforeEach
+    fun setup() {
+        wireMockServer.resetAll()
+
+    }
+
+    @AfterAll
+    fun afterAll() {
+        wireMockServer.stop()
+    }
+
+
+    @Test
+    fun `verify basic auth is on the request`() {
+        stubFor(
+            get(urlEqualTo("/ping"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withBody("""{"ping": "pong"}""")
+                )
+        )
+
+        client.ping().get()
+
+        val expectedCredentials = BasicCredentials(ORG_ID, API_KEY)
+        verify(getRequestedFor(urlEqualTo("/ping")).withBasicAuth(expectedCredentials))
+    }
+
+}
