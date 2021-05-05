a34c48b33b78d76cb11cb8b8b9e058d0e0160f31
Leah Thompson <lthompson@classpass.com>
2021-05-05 12:11:38 -0600

    let's compile to java 11 since 13 is eol

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,6 +71,8 @@ application {
 apply(plugin = "maven-publish")
 
 java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
     withSourcesJar()
 }
 
