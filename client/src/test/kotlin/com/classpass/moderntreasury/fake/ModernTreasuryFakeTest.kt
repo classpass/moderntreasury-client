43e61f6b2137b7532e2f7e4665ed81e1736ffe80
Jon Stafford <jstafford@classpass.com>
2021-06-08 10:15:54 -0600

    Throw version exception on conflict

--- client/src/test/kotlin/com/classpass/moderntreasury/fake/ModernTreasuryFakeTest.kt
+++ client/src/test/kotlin/com/classpass/moderntreasury/fake/ModernTreasuryFakeTest.kt
@@ -1,6 +1,7 @@
 package com.classpass.moderntreasury.fake
 
 import com.classpass.moderntreasury.client.ModernTreasuryClient
+import com.classpass.moderntreasury.exception.LedgerAccountVersionConflictException
 import com.classpass.moderntreasury.exception.ModernTreasuryApiException
 import com.classpass.moderntreasury.model.LedgerEntryDirection
 import com.classpass.moderntreasury.model.LedgerTransactionStatus
@@ -8,6 +9,7 @@ import com.classpass.moderntreasury.model.NormalBalanceType
 import com.classpass.moderntreasury.model.request.RequestLedgerEntry
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -70,6 +72,23 @@ class ModernTreasuryFakeTest {
     }
 
     @Test
+    fun `An exception is thrown when creating a transaction with an outdated lock version`() {
+        val debit = RequestLedgerEntry(100, LedgerEntryDirection.DEBIT, usd_cash.id, 0)
+        val credit = RequestLedgerEntry(100, LedgerEntryDirection.CREDIT, us_venue.id, 0)
+
+        // Will succeed because the lockVersion matches
+        client.createLedgerTransaction(debit, credit, status = LedgerTransactionStatus.POSTED)
+
+        try {
+            // Should fail because the lock version increased when the previous ledger transaction was created
+            client.createLedgerTransaction(debit, credit, status = LedgerTransactionStatus.POSTED)
+            fail("Should have caught LedgerAccountVersionConflictException")
+        } catch (ee: ExecutionException) {
+            assertTrue(ee.cause is LedgerAccountVersionConflictException)
+        }
+    }
+
+    @Test
     fun `Supports idempotent transaction creation`() {
         val KEY = "Supports idempotent transaction creation"
         val debit = RequestLedgerEntry(100, LedgerEntryDirection.DEBIT, usd_cash.id)
@@ -122,12 +141,15 @@ fun assertApiException(errorMessage: String, it: () -> Unit) {
     assertEquals(exception?.errorMessage, errorMessage)
 }
 
-fun ModernTreasuryClient.createLedgerTransaction(vararg entries: RequestLedgerEntry) =
+fun ModernTreasuryClient.createLedgerTransaction(
+    vararg entries: RequestLedgerEntry,
+    status: LedgerTransactionStatus = LedgerTransactionStatus.PENDING
+) =
     this.createLedgerTransaction(
         TODAY,
         entries.toList(),
         "Automatic:${nextId++}",
         "",
-        LedgerTransactionStatus.PENDING,
+        status,
         ""
     ).get()
