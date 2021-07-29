9aa54c7c0337011848257b617b9522d622eb8bc5
Zander Wilson-Thompson <zwilson-thompson@classpass.com>
2021-07-29 15:42:59 -0600

    Add DateQuery params to getLedgerTransactions

--- client/src/main/kotlin/com/classpass/moderntreasury/client/ModernTreasuryClient.kt
+++ client/src/main/kotlin/com/classpass/moderntreasury/client/ModernTreasuryClient.kt
@@ -12,6 +12,7 @@ import com.classpass.moderntreasury.model.NormalBalanceType
 import com.classpass.moderntreasury.model.request.CreateLedgerAccountRequest
 import com.classpass.moderntreasury.model.request.CreateLedgerRequest
 import com.classpass.moderntreasury.model.request.CreateLedgerTransactionRequest
+import com.classpass.moderntreasury.model.request.DateQuery
 import com.classpass.moderntreasury.model.request.RequestLedgerEntry
 import com.classpass.moderntreasury.model.request.RequestMetadata
 import com.classpass.moderntreasury.model.request.UpdateLedgerTransactionRequest
@@ -71,7 +72,10 @@ interface ModernTreasuryClient : Closeable {
         /**
          * Key/Value metadata pairs to search transactions for.
          */
-        metadata: Map<String, String> = emptyMap()
+        metadata: Map<String, String> = emptyMap(),
+        effectiveDate: DateQuery? = null,
+        postedAt: DateQuery? = null,
+        updatedAt: DateQuery? = null
     ): CompletableFuture<ModernTreasuryPage<LedgerTransaction>>
 
     fun createLedgerTransaction(
